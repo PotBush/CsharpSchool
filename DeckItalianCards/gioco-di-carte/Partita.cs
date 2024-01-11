0d78813634c004c48841332cafbeca086ae476cf
PotBush <potbush@proton.me>
2024-01-11 17:54:05 +0100

    comit

---
+++ DeckItalianCards/gioco-di-carte/Partita.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gioco_di_carte
+{
+    public class Partita
+    {
+        public enum DecretoPartita
+        {
+            VittoriaGiocatore1,
+            VittoriaGiocatore2,
+            Pareggio
+        }
+
+        private Mazzo _mazzo;
+        private Giocatore _giocatore1;
+        private Giocatore _giocatore2;
+        private Semi _briscola;
+
+        public Mazzo Mazzo
+        {
+            get { return _mazzo; }
+        }
+
+        public Giocatore Giocatore2
+        {
+            get { return _giocatore2; }
+        }
+
+        public Giocatore Giocatore1
+        {
+            get { return _giocatore1; }
+        }
+
+        public Semi Briscola
+        {
+            get { return _briscola; }
+        }
+
+
+
+        public Partita(string giocatore1, string giocatore2)
+        {
+            _mazzo = new Mazzo();
+            _mazzo.MescolaMazzo();
+            _giocatore1 = new Giocatore(giocatore1);
+            _giocatore2 = new Giocatore(giocatore2);
+            InizializzaPartita();
+
+       
+        }
+
+        public Carta[] DistribiusciCarte()
+        {
+            Carta []tempMano= new Carta[3];
+            
+            for (int i = 0; i < 3; i++)
+            {
+                tempMano[i] = Mazzo.EstraiPrimaCarta;
+            }
+            return tempMano;
+
+        }
+
+        private void InizializzaPartita()
+        {
+            Giocatore1.CarteGiocatore = DistribiusciCarte();
+            Giocatore2.CarteGiocatore = DistribiusciCarte();
+            Carta carta = _mazzo.VediPrimaCarta;
+            _briscola = carta.Seme;
+            _mazzo.Shift();
+        }
+
+        public int VincitoreMano(Carta carta1, Carta carta2)
+        {
+            int giocatoreVincente;
+
+            if (carta1.Seme == carta2.Seme)
+            {
+                if (carta1.Valore >  carta2.Valore)
+                {
+                    giocatoreVincente=1;
+
+                }else
+                {
+                    giocatoreVincente=2;
+                }
+
+            }else if (carta2.Seme == _briscola)
+            {
+                giocatoreVincente=2;
+            }
+            else 
+            {
+                giocatoreVincente = 1;
+            }
+            
+
+            if(giocatoreVincente==1) 
+            {
+                _giocatore1.AggiornaPunteggio(carta1);
+                _giocatore1.AggiornaPunteggio(carta2);
+            }
+            else
+            {
+                _giocatore2.AggiornaPunteggio(carta1);
+                _giocatore2.AggiornaPunteggio(carta2);
+            }
+
+            return giocatoreVincente;
+        }
+
+        public void PescaCarta(int giocatoreVincente)
+        {
+            if(giocatoreVincente == 1)
+            {
+                _giocatore1.InserisciCarta(_mazzo.EstraiPrimaCarta);
+                _giocatore2.InserisciCarta(_mazzo.EstraiPrimaCarta);
+            }else
+            {
+                _giocatore2.InserisciCarta(_mazzo.EstraiPrimaCarta);
+                _giocatore1.InserisciCarta(_mazzo.EstraiPrimaCarta);
+            }
+        }
+
+        public DecretoPartita EsitoPartita()
+        {
+            if (_giocatore1.PunteggioGiocatore > _giocatore2.PunteggioGiocatore)
+            {
+                return DecretoPartita.VittoriaGiocatore1;
+
+            }else if(_giocatore2.PunteggioGiocatore> _giocatore1.PunteggioGiocatore)
+            {
+                return DecretoPartita.VittoriaGiocatore2;
+
+            }else
+            {
+                return DecretoPartita.Pareggio;
+            }
+        }
+    }
+}
---
+++ DeckItalianCards/gioco-di-carte/Program.cs
@@ -0,0 +1,95 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace gioco_di_carte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci il nome del primo giocatore");
+            string nomeGiocatore1= Console.ReadLine();
+
+            Console.WriteLine("inserisci il nome del secondo giocatore");
+            string nomeGiocatore2 = Console.ReadLine();
+
+            Partita partita = new Partita(nomeGiocatore1,nomeGiocatore2);
+            Console.WriteLine(partita.Briscola);
+            int giocatoreVincente;
+            do
+            {
+                Carta cartaG1;
+                int cartaSceltaG1;
+                do
+                {
+                    Console.WriteLine("Le carte del giocatore 1 sono:");
+                    for (int i = 0; i < 3; i++)
+                    {
+                        if (partita.Giocatore1.CarteGiocatore[i] != null)
+                        {
+                            Console.WriteLine($"Le carta{i}: {partita.Giocatore1.CarteGiocatore[i].ToString()}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Le carta{i}: è gia usata");
+                        }
+                    }
+
+                    Console.WriteLine("Scegli la carta da lanciare 0 1 o 2?");
+                    cartaSceltaG1 = Convert.ToInt32(Console.ReadLine());
+
+                } while (partita.Giocatore1.CarteGiocatore[cartaSceltaG1] == null);
+
+
+                cartaG1 = partita.Giocatore1.SceltaManoGiocatore(cartaSceltaG1);
+
+
+
+                Carta cartaG2;
+                int cartaSceltaG2;
+                do
+                {
+                    Console.WriteLine("Le carte del giocatore 2 sono:");
+                    for (int i = 0; i < 3; i++)
+                    {
+                        if (partita.Giocatore2.CarteGiocatore[i] != null)
+                        {
+                            Console.WriteLine($"Le carta{i}: {partita.Giocatore2.CarteGiocatore[i].ToString()}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Le carta{i}: è gia usata");
+                        }
+                    }
+
+                    Console.WriteLine("Scegli la carta da lanciare 0 1 o 2?");
+                    cartaSceltaG2 = Convert.ToInt32(Console.ReadLine());
+
+                } while (partita.Giocatore2.CarteGiocatore[cartaSceltaG2] == null);
+
+
+                cartaG2 = partita.Giocatore2.SceltaManoGiocatore(cartaSceltaG2);
+
+                if (partita.Mazzo.Carte[partita.Mazzo.Carte.Length - 1] == null && partita.Mazzo.Carte[partita.Mazzo.Carte.Length-2] == null)
+                {
+                    int vincitore = partita.VincitoreMano(cartaG1, cartaG2);
+                    Console.WriteLine($"ha vinto il giocatore{vincitore}");
+                }
+                else
+                {
+                    int vincitore = partita.VincitoreMano(cartaG1, cartaG2);
+                    Console.WriteLine($"ha vinto il giocatore{vincitore}");
+                    partita.PescaCarta(vincitore);
+                }
+
+
+            } while (partita.Giocatore1.CarteFinite() == false && partita.Giocatore2.CarteFinite() == false);
+
+
+            
+
+            Console.WriteLine(partita.EsitoPartita());
+
+        }
+        
+    }
+}
