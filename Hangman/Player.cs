e95e08d97ea47e0e08732cb38c0f087e95712478
PotBush <potbush@proton.me>
2024-03-03 22:27:27 +0100

    update redme and upload hangman program

---
+++ Hangman/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    internal class Player
+    {
+        private string _name;
+        private static int max_lives = 11;
+    }
+}
