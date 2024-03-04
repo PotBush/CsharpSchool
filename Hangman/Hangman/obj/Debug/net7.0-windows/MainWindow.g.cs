ed9ceda6017f3138327de7babc99f55d82ee1674
PotBush <potbush@proton.me>
2024-03-05 01:17:45 +0900

    hagman

--- Hangman/Hangman/obj/Debug/net7.0-windows/MainWindow.g.cs
+++ Hangman/Hangman/obj/Debug/net7.0-windows/MainWindow.g.cs
@@ -253,7 +253,7 @@ namespace Hangman {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Hangman;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Hangman;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
